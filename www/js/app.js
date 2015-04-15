2d0c68a6ae0383813cd8a30322c4c8b21f042d14
Michael Allan <michael.allan@anz.com>
2015-04-15 14:56:03 +1000

    Setup routing and stub templates

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,25 @@
-var payd = angular.module('payd', ['nvd3']);
+var payd = angular.module('payd', ['ui.router', 'nvd3']);
+
+payd.config(function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html'
+    })
+    .state('transactions', {
+      url: '/transactions',
+      templateUrl: 'templates/transactions.html'
+    })
+    .state('analyse', {
+      url: '/analyse',
+      templateUrl: 'templates/analyse.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'templates/about.html'
+    });
+
+});
