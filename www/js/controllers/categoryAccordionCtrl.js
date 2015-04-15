ebaa2c8098feb13e241c87894be577d2a2259f75
Michael Allan <michael.allan@anz.com>
2015-04-15 20:57:53 +1000

    Add category accordion to home page

--- www/js/controllers/categoryAccordionCtrl.js
+++ www/js/controllers/categoryAccordionCtrl.js
@@ -1,16 +1,48 @@
-payd.controller('CategoryAccordionCtrl', function ($scope) {
+payd.controller('CategoryAccordionCtrl', function ($scope, $sce) {
   $scope.oneAtATime = true;
 
-  $scope.groups = [
+  $scope.categories = [
     {
-      title: 'Dynamic Group Header - 1',
+      title: 'Rent / Mortgage',
+      iconHtml: '<i class="fa fa-home"></i>',
       content: 'Dynamic Group Body - 1'
     },
     {
-      title: 'Dynamic Group Header - 2',
+      title: 'Utilities',
+      iconHtml: '<i class="fa fa-lightbulb-o"></i>',
+      content: 'Dynamic Group Body - 2'
+    },
+    {
+      title: 'Super',
+      iconHtml: '<i class="fa fa-glove"></i>',
+      content: 'Dynamic Group Body - 2'
+    },
+    {
+      title: 'Car Loan',
+      iconHtml: '<i class="fa fa-car"></i>',
+      content: 'Dynamic Group Body - 2'
+    },
+    {
+      title: 'Memberships',
+      iconHtml: '<i class="fa fa-users"></i>',
+      content: 'Dynamic Group Body - 2'
+    },
+    {
+      title: 'Medical',
+      iconHtml: '<i class="fa fa-medkit"></i>',
+      content: 'Dynamic Group Body - 2'
+    },
+    {
+      title: 'Entertainment',
+      iconHtml: '<i class="fa fa-ticket"></i>',
       content: 'Dynamic Group Body - 2'
     }
   ];
+
+  // Sadly, no go :-(
+  $scope.trust = function (i) {
+    return $sce.trustAsHtml($scope.categories[i].iconHtml);
+  };
 
   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
