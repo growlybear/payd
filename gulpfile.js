6d588637a804310e92a00569bf8ac7f1ef8e12b2
Steven Miller <steven.miller@anz.com>
2015-04-16 23:16:10 +1000

    LiveReload for html + css

--- gulpfile.js
+++ gulpfile.js
@@ -8,4 +8,19 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default', ['connect']);
+gulp.task('html', function () {
+  gulp.src('./www/**/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  gulp.src('./www/css/*.css')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./www/*.html', './www/templates/*.html'], ['html']);
+  gulp.watch(['./www/css/*.css'], ['css']);
+});
+
+gulp.task('default', ['connect', 'watch']);
