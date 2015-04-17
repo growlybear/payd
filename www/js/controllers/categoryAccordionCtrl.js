c2e32202db6c83a6f4860aa7c88b20d3bdc0a136
Steven Miller <steven.miller@anz.com>
2015-04-17 10:18:01 +1000

    Transaction initial

--- www/js/controllers/categoryAccordionCtrl.js
+++ www/js/controllers/categoryAccordionCtrl.js
@@ -1,47 +1,89 @@
 payd.controller('CategoryAccordionCtrl', function ($scope, $sce) {
   $scope.oneAtATime = true;
 
+  var transactions = [
+    {
+      "id": 0.35290041542612016,
+      "date": "4 Aug",
+      "description": "Rerum quis sed reprehenderit et quia doloremque.",
+      "category": "Car Loan",
+      "tags": [
+        "leisure"
+      ],
+      "amount": 580,
+      "fromUser": "Michael Allan",
+      "toUser": "Miss Wilburn Armstrong",
+      "merchant": 8045
+    },
+    {
+      "id": 0.5906417586375028,
+      "date": "4 Feb",
+      "description": "Culpa tempore incidunt error vero vel esse autem possimus.",
+      "category": "Memberships",
+      "tags": [
+        "fitness"
+      ],
+      "amount": 11,
+      "fromUser": "Michael Allan",
+      "toUser": "Mrs. Maynard Green",
+      "merchant": 747
+    },
+  ];
+
   $scope.categories = [
     {
       title: 'Rent / Mortgage',
-      content: 'Dynamic Group Body - 1'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Super',
       content: {
-        transactions: [{
-          name: 'Thing',
-          amount: '123'
-        }]
+        transactions: transactions
       }
     },
     {
       title: 'Utilities',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Car Loan',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Membership',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Entertainment',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Medical',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Misc',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     },
     {
       title: 'Dots',
-      content: 'Dynamic Group Body - 2'
+      content: {
+        transactions: transactions
+      }
     }
   ];
 
